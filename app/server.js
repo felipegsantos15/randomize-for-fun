ad7653d061f70e1fae8460828bfd0a48299f533d
felipe.santos <felipe.santos@pontaltelecom.com.br>
2020-04-04 16:58:42 -0300

    adcionado body parser

--- app/server.js
+++ app/server.js
@@ -3,6 +3,7 @@ const router = require('./router');
 const server = restify.createServer();
 
 router.router(server);
+server.use(restify.plugins.bodyParser());
 server.listen(process.env.PORT, () => {
     console.log(`server listen on port ${process.env.PORT}`);
 });
